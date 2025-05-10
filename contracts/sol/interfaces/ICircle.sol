91043b510567141653595dbb547d5bf80c46609d
Joera <joera@joeramulders.com>
2025-05-10 10:28:18 +0200

    morning merge

--- contracts/sol/interfaces/ICircle.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface ICircle {
-    function mint(address to, uint256 amount) external;
-    function burn(address from, uint256 amount) external;
-}
